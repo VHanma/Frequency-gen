13597c572ec202387cb580cf2d3ecfb932a9976e
VHanma <morgandeavon@gmail.com>
2026-08-25 13:25:45 -0500

    Bump Frequency Remapper to v1.5.0

--- FrequencyRemapper/app/build.gradle.kts
+++ FrequencyRemapper/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vaan.frequencyremapper"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.4.0"
+        versionCode = 8
+        versionName = "1.5.0"
     }
 
     buildTypes {
